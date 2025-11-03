328e65d0b2f1161cc12bb9598a0d340ea8b27f74
angel vargas <angelvargascoloma2004@gmail.com>
2025-11-03 00:35:53 -0500

    videos y correcionees

--- script-menu.js
+++ script-menu.js
@@ -1,3 +1,34 @@
 function volverInicio() {
   window.location.href = "index.html";
 }
+function toggleOpciones() {
+  const opciones = document.getElementById("opcionesCompartir");
+  opciones.style.display = opciones.style.display === "flex" ? "none" : "flex";
+}
+
+function toggleQR() {
+  const qr = document.getElementById("qrImage");
+  qr.style.display = qr.style.display === "none" || qr.style.display === "" ? "block" : "none";
+}
+
+function copiarLink() {
+  const link = window.location.href; // copia el link actual de la página
+  navigator.clipboard.writeText(link).then(() => {
+    alert("🔗 Link copiado al portapapeles!");
+  });
+}
+
+function descargarQR() {
+  const enlace = document.getElementById("linkDescargaQR");
+  enlace.click();
+}
+
+function compartirFacebook() {
+  const link = encodeURIComponent(window.location.href);
+  window.open(`https://www.facebook.com/profile.php?id=61575278036063`, "_blank");
+}
+
+function compartirWhatsApp() {
+  const link = encodeURIComponent(window.location.href);
+  window.open(`https://wa.me/593967944885?text=${texto}`, "_blank");
+}
